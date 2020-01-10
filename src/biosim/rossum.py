b40d8163b6a47d0a58adf97ddf0b9d9086572ac9
Markus Ola Granheim <mgranhei@nmbu.no>
2020-01-10 17:45:23 +0100

    writing test and code for savannah

--- src/biosim/rossum.py
+++ src/biosim/rossum.py
@@ -11,7 +11,8 @@ import numpy as np
 
 
 class Island:
-    def __init__(self, le_map = None):
+    def __init__(self, le_map=None):
+        """Sets the variables in the Island-class"""
         self.valid_map_vals = ['O', 'D', 'M', 'S', 'J']
         self.le_map = le_map
         if self.le_map is None:
@@ -21,13 +22,13 @@ class Island:
         Island.string_to_matrix(self)
 
     def string_to_matrix(self):
+        """Converts the input multiline-string to a matrix"""
         if type(self.le_map) is not str:
             raise TypeError('Input needs to be a string')
-
         list1 = self.le_map.split()
         list2 = []
         for i in range(len(list1)):
-            list2 += [a for a in list1[i]]
+            list2 += [hc for hc in list1[i]]
 
         self.col = int(len(list2) / len(list1))
         self.rader = int(len(list1))
@@ -35,7 +36,7 @@ class Island:
         self.le_map = np.reshape(list2, (self.col, self.rader))
 
     def limit_map_vals(self):
-
+        """Raises ValueErrors if the input island-string violates any of the criterions for the island"""
         i_max = self.rader - 1
         j_max = self.col - 1
 
@@ -48,36 +49,125 @@ class Island:
                         raise ValueError('One or more of the perimeter-tiles are not ocean')
 
     def fetch_map(self):
+        """Returns the map"""
         return self.le_map
 
     def fetch_naturetype(self, pos):
+        """Fetches the naturetype of the map in the input position"""
         return self.le_map[pos]
 
 
-class Savannah(Island):
-    def __init__(self, fsav_max=None, alpha=None):
+class Savannah:
+    """Class for the savannah tiles"""
+    def __init__(self, fsav_max=None, alpha=None, f=None):
+        self.food = {}
         if fsav_max is None:
             self.fsav_max = 300
-        self.fsav_max = fsav_max
+        else:
+            self.fsav_max = fsav_max
+        if f is None:
+            self.f = 10
+        else:
+            self.f = f
+
         if alpha is None:
             self.alpha = 0.3
+        else:
+            self.alpha = alpha
 
-    def set_food(self):
-        return self.fsav_max
+    def set_food(self, pos):
+        self.food.update({pos: self.fsav_max})
 
-    def update_food(self, f_ij=None):
-        if f_ij is None:
-            raise ValueError
-        return f_ij + self.alpha * (self.fsav_max - f_ij)
+    def grow_food(self, pos):
+        self.food.update({pos: self.food[pos] + self.alpha * (self.fsav_max - self.food[pos])})
+
+    def food_gets_eaten(self, pos):
+        if self.f <= self.food[pos]:
+            self.food[pos] -= self.f
+            return self.f
+        elif self.food == 0:
+            return 0
+        else:
+            b = self.food[pos]
+            self.food[pos] = 0
+            return b
 
 
-class Jungle(Island):
-    pass
+class Jungle:
+    def __init__(self, f_jungle=None):
+        self.food = {}
+        self.f_jungle = f_jungle
+        if self.f_jungle is None:
+            self.f_jungle = 800
+
+    def update_food(self, pos):
+        self.food[pos] = self.f_jungle
 
 
-class Mountain_ocean(Island):
-    pass
+class Desert:
+    def __init__(self):
+        self.food_animals_data = {}
+
+    def set_food(self, pos):
+        pass
 
 
-class Desert(Island):
-    pass
+class Animals:
+    def __init__(self):
+        pass
+
+
+class Herbivores:
+    def __init__(self, w_birth=8.0, sigma_birth=8.0, beta=1.5, eta=0.05, a_half=40.0, phi_age=0.2, w_half=10.0,
+                 phi_weight=0.1, mu=0.25, lambda1=1.0, gamma=0.2, zeta=3.5, xi=1.2, omega=0.4):
+        self.w_birth = w_birth
+        self.sigma_birth = sigma_birth
+        self.beta = beta
+        self.eta = eta
+        self.a_half = a_half
+        self.phi_age = phi_age
+        self.w_half = w_half
+        self.phi_weight = phi_weight
+        self.mu = mu
+        self.lambda1 = lambda1
+        self.gamma = gamma
+        self.zeta = zeta
+        self.xi = xi
+        self.omega = omega
+        self.herbs = {}
+
+    def add_animals(self, new_animals):
+        self.herbs = {self.herbs, new_animals}
+
+    def calculate_fitness(self, pos):
+        for animal in self.herbs[pos]:
+            if animal['weight'] == 0:
+                new_fitness = {'fitness': 0}
+                animal.update(new_fitness)
+            else:
+                new_fitness = {'fitness': (1 / (1 + np.exp(self.phi_age * (animal['age'] - self.a_half)))) *
+                                          (1 / (1 + np.exp(self.phi_weight * (animal['weight'] - self.w_half))))}
+                animal.update(new_fitness)
+
+    def sort_by_fitness(self, pos):
+        pass
+
+    def animals_eat(self, pos):
+        pass
+
+    def breeding(self, pos):
+        pass
+
+    def aging(self, pos):
+        pass
+
+    def loss_of_weight(self, pos):
+        pass
+
+    def death(self):
+        pass
+
+
+class Carnivores:
+    def __init__(self):
+        pass
--- tests/test_rossum.py
+++ tests/test_rossum.py
@@ -7,28 +7,28 @@ This file will test the rossum.py-file
 __author__ = "Markus Ola Granheim & Rasmus Svebestad"
 __email__ = "mgranhei@nmbu.no & rasmus.svebestad@nmbu.no"
 
-from biosim.rossum import island
+from biosim.rossum import Island, Savannah
 import pytest
 import numpy as np
 
 
 class TestIsland:
-    def test_island_boarder(self):
+    def test_island_call(self):
         """Default constructor callable"""
-        a = island()
-        assert isinstance(a, island)
+        a = Island()
+        assert isinstance(a, Island)
 
     def test_perimeter_error(self):
         """Testing if ValueError is raised when the perimeter contains something other than ocean"""
         a = "OOO\nOJJ\nOOO"
-        aa = island(a)
+        aa = Island(a)
         with pytest.raises(ValueError):
             aa.limit_map_vals()
 
     def test_unvalid_terrain_error(self):
         """Tests if the map input contains unvalid terrain-types"""
         a = "OOO\nOWO\nOOO"
-        aa = island(a)
+        aa = Island(a)
         with pytest.raises(ValueError):
             aa.limit_map_vals()
 
@@ -36,17 +36,83 @@ class TestIsland:
         """Tests if the code raises TypeError if the map input is not a string"""
         a = [['O', 'O', 'O'], ['O', 'J', 'O'], ['O', 'O', 'O']]
         with pytest.raises(TypeError):
-            island(a)
+            Island(a)
 
     def test_output_np_array(self):
         """Tests if the output map is right, when input has no error"""
         a = "OOO\nOJO\nOOO"
-        aa = island(a)
+        aa = Island(a)
         el_map = [['O', 'O', 'O'], ['O', 'J', 'O'], ['O', 'O', 'O']]
         assert np.array_equal(aa.fetch_map(), el_map)
 
     def test_return_neturtype(self):
         a = "OOO\nOJO\nOOO"
-        aa = island(a)
-        pos = (0,0)
+        aa = Island(a)
+        pos = (0, 0)
         assert aa.fetch_naturetype(pos) == 'O'
+
+
+class TestSavannah:
+    def test_savannah_call(self):
+        """Default constructor callable"""
+        a = Savannah()
+        assert isinstance(a, Savannah)
+
+    def test_default_values(self):
+        a = Savannah()
+        assert a.fsav_max == 300
+        assert a.alpha == 0.3
+
+    def test_values(self):
+        a = Savannah(fsav_max=200, alpha=0.4)
+        assert a.fsav_max == 200
+        assert a.alpha == 0.4
+
+    def test_set_food(self):
+        a = Savannah(fsav_max=300)
+        a.set_food((1, 3))
+        assert a.food == {(1, 3): 300}
+
+    def test_set_more_food(self):
+        a = Savannah(fsav_max=300)
+        for i in range(3):
+            for j in range(3):
+                a.set_food((i, j))
+        assert a.food[(0, 2)] == 300
+        assert a.food[(1, 1)] == 300
+
+    def test_grow_food(self):
+        a = Savannah(fsav_max=300, alpha=0.3)
+        a.set_food((1, 1))
+        a.grow_food((1, 1))
+        assert a.food[1, 1] == (300 + 0.3 * (300 - 300))
+
+    def test_multiple_grow_food(self):
+        a = Savannah()
+        for i in range(3):
+            for j in range(3):
+                a.set_food((i, j))
+        for i in range(3):
+            for j in range(3):
+                a.grow_food((i, j))
+        assert a.food[0, 0] == (300 + 0.3 * (300 - 300))
+        assert a.food[1, 1] == (300 + 0.3 * (300 - 300))
+        assert a.food[2, 2] == (300 + 0.3 * (300 - 300))
+        assert a.food[1, 2] == (300 + 0.3 * (300 - 300))
+
+    def test_food_gets_eaten_with_less_food(self):
+        a = Savannah(fsav_max=0)
+        b = Savannah(fsav_max=5)
+        c = Savannah()
+        a.set_food((1, 1))
+        b.set_food((1, 2))
+        c.set_food((2, 2))
+        aa = a.food_gets_eaten((1, 1))
+        bb = b.food_gets_eaten((1, 2))
+        cc = c.food_gets_eaten((2, 2))
+        assert aa == 0
+        assert a.food[(1, 1)] == 0
+        assert bb == 5
+        assert b.food[(1, 2)] == 0
+        assert cc == 10
+        assert c.food[(2, 2)] == 290
